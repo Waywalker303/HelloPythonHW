4ac1a8b50c92ad53b5b7c0414cfefb06fcf6ec22
vasil <ip.vasiliy@bk.ru>
2022-09-20 18:46:03 +0400

    add homeWork sem 4

---
+++ sem_4.2_HW_list_simple_N.py
@@ -0,0 +1,17 @@
+# Задайте натуральное число N. Напишите программу, 
+# которая составит список простых множителей числа N.
+
+
+number = int(input("Введите число: "))
+i = 2
+lst = []
+old = number
+while i <= number:
+    if number % i == 0:
+        lst.append(i)
+        number //= i
+        i = 2
+    else:
+        i += 1
+        
+print(f"Простые множители числа {old} = {lst}")
